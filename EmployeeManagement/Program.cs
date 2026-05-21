1764690a4c44152c3b4f5a26796b3553e9e3e69f
CrimsonCompiler <toisiftasrik@gmail.com>
2026-05-21 12:59:07 +0600

    Add Swagger support for API documentation in Program.cs

--- EmployeeManagement/Program.cs
+++ EmployeeManagement/Program.cs
@@ -9,7 +9,20 @@ namespace EmployeeManagement
             // 1. Dependecny Injection 
             builder.Services.AddControllers();
 
+            // Swagger
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+
             var app = builder.Build();
+
+            // Swagger UI Load
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Employee API V1");
+            });
+
 
             // 2. Http Request Routing middleware active
             app.UseRouting();
