5281d3eceab68bdd89368e2fadbe1e83478008b0
CrimsonCompiler <toisiftasrik@gmail.com>
2026-05-21 10:59:29 +0600

    Implement dependency injection and configure HTTP request routing in Program.cs

--- EmployeeManagement/Program.cs
+++ EmployeeManagement/Program.cs
@@ -5,7 +5,17 @@ namespace EmployeeManagement
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            // 1. Dependecny Injection 
+            builder.Services.AddControllers();
+
             var app = builder.Build();
+
+            // 2. Http Request Routing middleware active
+            app.UseRouting();
+
+            // 3. Mapping the controllers endpoint
+            app.MapControllers();
 
             app.MapGet("/", () => "Hello World!");
 
