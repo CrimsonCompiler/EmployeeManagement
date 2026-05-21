0fc306bb1dbb10cac148ee2eccddaad36a301df2
CrimsonCompiler <toisiftasrik@gmail.com>
2026-05-21 11:41:02 +0600

    Add EmployeesController with Index action method

---
+++ EmployeeManagement/Controllers/EmployeesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EmployeeManagement.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EmployeeManagement.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
