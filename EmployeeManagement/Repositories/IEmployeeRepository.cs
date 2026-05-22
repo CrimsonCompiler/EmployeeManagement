54f5a344e0290c2325baf612ec5a141f2bde5aa8
CrimsonCompiler <toisiftasrik@gmail.com>
2026-05-22 06:01:47 +0600

    Add IEmployeeRepository interface for employee data operations

---
+++ EmployeeManagement/Repositories/IEmployeeRepository.cs
@@ -0,0 +1,13 @@
+﻿using EmployeeManagement.Models;
+
+namespace EmployeeManagement.Repositories
+{
+    public interface IEmployeeRepository
+    {
+        IEnumerable<Employee> GetAll();
+        Employee GetById(int id);
+        int Create(Employee employee);
+        bool Update(Employee employee);
+        bool Delete(int id);
+    }
+}
