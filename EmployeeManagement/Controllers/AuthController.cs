c9dd25641559fc1d35f310fc3218bbe8707f1581
CrimsonCompiler <toisiftasrik@gmail.com>
2026-05-24 23:56:17 +0600

    Add AuthController and LoginModel for JWT authentication

---
+++ EmployeeManagement/Controllers/AuthController.cs
@@ -0,0 +1,34 @@
+﻿using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagement.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IConfiguration  _configuration;
+
+
+        // Using DI to read JWT settings
+        public AuthController(IConfiguration configuration)
+        {
+               _configuration = configuration;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginModel login)
+        {
+            if(login.Username == "admin" && login.Password == "1234")
+            {
+                var tokenString = GenerateJwtToken(login.Username);
+                return Ok(new { Token = tokenString });
+            }
+
+            return Unauthorized("Invalid Credentials");
+        }
+
+
+
+    }
+}
