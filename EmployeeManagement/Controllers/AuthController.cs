09e223f8f84d7d6f5379bd8511e9dcf6a39c1290
CrimsonCompiler <toisiftasrik@gmail.com>
2026-05-25 00:04:48 +0600

    Add GenerateJwtToken method to AuthController for JWT token creation

--- EmployeeManagement/Controllers/AuthController.cs
+++ EmployeeManagement/Controllers/AuthController.cs
@@ -1,5 +1,9 @@
 ﻿using EmployeeManagement.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace EmployeeManagement.Controllers
 {
@@ -28,6 +32,20 @@ namespace EmployeeManagement.Controllers
             return Unauthorized("Invalid Credentials");
         }
 
+
+        private string GenerateJwtToken(string username)
+        {
+            // step - 1 Setup secret key and algorithm ( signature )
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JwtSettings:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            // Claims / Payload - store user information
+            var claims = new[] { 
+                new Claim(JwtRegisteredClaimNames.Sub, username),
+                new Claim(ClaimTypes.Role, "Admin"),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+        }
 
 
     }
