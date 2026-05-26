a7687839286401903cb0b8e5f6f291fb4703aad2
CrimsonCompiler <toisiftasrik@gmail.com>
2026-05-26 10:07:05 +0600

    Add User model and IUserRepository interface for user management

---
+++ EmployeeManagement/Repositories/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using EmployeeManagement.Models;
+
+namespace EmployeeManagement.Repositories
+{
+    public interface IUserRepository
+    {
+        User GetUserCredentials(string username, string password);
+    }
+}
